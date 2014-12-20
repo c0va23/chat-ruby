0f4030a53f292742f184f383e94ee2b229422074
Dmitrij Fedorenko <c0va23@gmail.com>
2014-12-20 19:34:11 +0300

    GET /messages return all old messages.

--- app.rb
+++ app.rb
@@ -1,17 +1,41 @@
 require "bundler"
 Bundler.require(:default)
 
-Message = Struct.new(:text, :time)
+class Message < Struct.new(:id, :text, :time)
+  @@message_id = 0
+
+  def initialize(text)
+    @@message_id += 1
+    self.id = @@message_id
+    self.text = text
+    self.time = Time.now
+  end
+
+  def write_to(connection)
+    connection << "id:#{self.id}\ndata: #{self.to_h.to_json}\n\n"
+  end
+end
 
 connections = []
 messages = []
 
-def write_message(connection, message)
-  connection << "data: #{message.to_h.to_json}\n\n"
-end
-
 get "/messages", provides: "text/event-stream" do
+  last_event_id = request.env['HTTP_LAST_EVENT_ID']
+
+  undelivered_messages = if last_event_id
+    last_message_index = messages.find_index { |m| m.id == last_event_id.to_i }
+    if last_message_index && last_message_index < messages.size
+      messages[last_message_index+1..-1]
+    end
+  else
+    messages
+  end
+
   stream :keep_open do |connection|
+    messages.each do |message|
+      message.write_to(connection)
+    end
+
     connections << connection
     connection.callback do
       connections.delete(connection)
@@ -22,11 +46,11 @@ end
 post "/messages" do
   message_data = JSON.parse(request.body.read)
 
-  message = Message.new(message_data['text'], Time.now)
+  message = Message.new(message_data['text'])
   messages << message
 
   connections.each do |connection|
-    write_message(connection, message)
+    message.write_to(connection)
   end
 
   message.to_h.to_json
